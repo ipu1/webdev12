ea3661b7c317f82e4510eb8370e68fc61791c28e
ipu1 <112542319+ipu1@users.noreply.github.com>
2022-11-23 11:26:31 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,26 @@
+let shopItemsData = [{
+    id:"Hattula",
+    name:"Hattulan Lintu",
+    price: 75,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/Hattula.jpeg",
+},{
+    id:"Eura",
+    name:"Euran Sydän",
+    price: 50,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/Euran.jpeg",
+},{
+    id:"Kuutar",
+    name:"Kuutar",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/Kuutar.jpeg",
+},{
+    id:"Roihu",
+    name:"Elämän Roihu",
+    price: 85,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/Roihu.jpeg",
+},
+];
